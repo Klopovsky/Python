86b420e0198c589ebf28ab8da4fc69e00b0f48e7
Klopovsky <xraydust@gmail.com>
2022-07-23 20:51:23 +0400

    add HAR file for parsing

--- try/test.py
+++ try/test.py
@@ -1,6 +1,9 @@
 from urllib import response
 import requests
 from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
+
+HEADER = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
 
 for url in ['https://api.github.com', 'https://api.github.com/invalid']:
     try:
@@ -9,7 +12,7 @@ for url in ['https://api.github.com', 'https://api.github.com/invalid']:
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
-        print(f'Unknown error: {err}')
+        print(f'Other error: {err}')
     else:
         print('Success!')
 
